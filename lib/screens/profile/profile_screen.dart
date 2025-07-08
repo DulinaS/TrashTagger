ccf6f4658edcfae118a118d355e6325331ac8ed3
Dulina Senarathne <dulinahansa2002@gmail.com>
2025-07-08 12:02:12 +0530

    Created badges screen. updated path from profile screen

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:trash_tagger/screens/profile/badges_screen.dart';
 import '../../providers/auth_provider.dart';
 import '../../providers/user_provider.dart';
 import '../../themes/app_theme.dart';
@@ -276,9 +277,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
               children: [
                 Text('Achievements', style: AppTheme.headlineMedium),
                 TextButton(
-                  onPressed: () {
-                    // TODO: Navigate to full badges screen
-                  },
+                  onPressed: () => _routeToBadgesScreen(),
                   child: const Text('View All'),
                 ),
               ],
@@ -571,6 +570,13 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
         ],
       ),
+    );
+  }
+
+  void _routeToBadgesScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => BadgesScreen()),
     );
   }
 
