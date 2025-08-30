f7879081e854d3362dfb46ec122d1a455900f54b
Dulina Senarathne <dulinahansa2002@gmail.com>
2025-08-31 01:09:24 +0530

    Updated index.ts to export all notification functions. Exist type error

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -2,7 +2,7 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
+//import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:cloud_functions/cloud_functions.dart';
